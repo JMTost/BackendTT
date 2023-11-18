4e31022ba29844a92e353405d1d7f7ad14de89c8
JMTost <max.55@live.com.mx>
2023-11-17 19:36:36 -0600

    eliminación de credenciales

--- correo.js
+++ correo.js
@@ -4,18 +4,7 @@ var opcionesCorreo = {
     from : 'app2bfit@gmail.com'
 }
 
-var transporter = nodemailer.createTransport({
-    service : 'gmail',
-    auth : {
-        type : 'OAuth2',
-        user : "app2bfit@gmail.com",
-        pass : "ContraPrueba_2bfit",
-        clientId : "515458168347-8af5r9et23g94ddjok6v6mnffpnnktbp.apps.googleusercontent.com",
-        clientSecret : "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",
-        refreshToken : "1//04wvo1y3ufqH3CgYIARAAGAQSNwF-L9Ir86_sIHo3gXROXXLPfTq-Yo5o9JzLRH0LFazmAFF_-iuKWYT5PDokeAsSVbBQYBamp_w"
-    }
-});
-/*
+
 var transporter = nodemailer.createTransport({
     service : 'gmail',
     auth : {
@@ -27,7 +16,7 @@ var transporter = nodemailer.createTransport({
         refreshToken : ""
     }
 });
-*/
+
 
 function crearOpcionesCorreo(correoDestino, titulo, texto){
     opcionesCorreo.to = correoDestino;
